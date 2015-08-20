f7a61fb403ba911fe307b9bf0f78281cfdc83bc4
takieyda <takieyda@gmail.com>
2015-08-20 02:10:30 -0400

    r/DailyProgrammer #228 Easy Letters in Alphabetical Order
    https://www.reddit.com/r/dailyprogrammer/comments/3h9pde/20150817_challenge_228_easy_letters_in/
    
    Working on this. I've got the list being iterated, but the list.sort()
    function isn't working like I want. Produces:
    
    ('billowy', 'NOT IN ORDER')
    ('begins', 'NOT IN ORDER')
    ('bijoux', 'NOT IN ORDER')
    ('billowy', 'NOT IN ORDER')
    ('biopsy', 'NOT IN ORDER')
    ('chimps', 'NOT IN ORDER')
    ('chinos', 'NOT IN ORDER')
    ('chintz', 'NOT IN ORDER')
    ('defaced', 'NOT IN ORDER')
    ('fiddle', 'NOT IN ORDER')
    ('sponged', 'NOT IN ORDER')
    ('wronged', 'NOT IN ORDER')

---
+++ dailyprogrammer_228_easy.py
@@ -0,0 +1,10 @@
+#Practicing challenge #228 Easy from r/dailyprogrammer
+
+list = ["billowy", "biopsy", "chinos", "defaced", "chintz", "sponged",\
+        "bijoux", "abhors", "fiddle", "begins", "chimps", "wronged"]
+
+for item in list:
+    if item == list.sort():
+        print(item, "IN ORDER")
+    else:
+        print(item, "NOT IN ORDER")
--- thezaynthe_problem.py
+++ thezaynthe_problem.py
@@ -1,25 +1,26 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = []
 
 #main
-for item in a:
-    if item < 5:
-        print item
+for item in a:          #iterate each item in list a
+    if item < 5:        #if the item less than 5
+        print(item)     #print the item, each on separate line
 
 #extra 2
-for item in a:
+b = []
+
+for item in a:          #same as before
     if item < 5:
-        b.append(item)
-print(b)
+        b.append(item)  #except appends the item to list b
+print(b)                #prints list b as a whole
         
 #extra 3
 num = raw_input("Number? ")
 
 c = []
 
-for item in a:
-    if item < int(num):
-        c.append(item)
+for item in a:          #same as before
+    if item < int(num): #checks the item against user input, as an integer
+        c.append(item)  #appends to list c
 print(c)
 
 #adreaofhodor comment
