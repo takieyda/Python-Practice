677e81b7ef05cc379736e5b9927134a3c9f90029
takieyda <takieyda@gmail.com>
2015-08-20 02:50:25 -0400

    There it is.
    Needed to use "".join(sorted(item)) to sort the item into a list and
    join those list items together. So: defaced becomes ['a', 'c', 'd', 'd',
    'e', 'e', 'f'], the join makes it acddeef, and defaced != acddeef.
    
    In the elif, reverse=True simply reverses defaced to decafed, and in
    this instance is not equal.
    
    Also, reminded of the importance of True and False being capitalized,
    otherwise they are parsed (is that the correct word?) as variables
    instead of boolean.
    
    And, concatenation. I was printing as such print(item, "IN ORDER) which
    would produce ('billowy', 'IN ORDER'). In order to get the output to
    match the challenges output, with a hint from a comment post, I found
    out that print(item + " IN ORDER") produces billowy IN ORDER without the
    extra characters.

--- dailyprogrammer_228_easy.py
+++ dailyprogrammer_228_easy.py
@@ -1,10 +1,13 @@
 #Practicing challenge #228 Easy from r/dailyprogrammer
 
-list = ["billowy", "biopsy", "chinos", "defaced", "chintz", "sponged",\
+list1 = ["billowy", "biopsy", "chinos", "defaced", "chintz", "sponged",\
         "bijoux", "abhors", "fiddle", "begins", "chimps", "wronged"]
 
-for item in list:
-    if item == list.sort():
-        print(item, "IN ORDER")
+for item in list1:
+    if item == "".join(sorted(item)):
+        print(item + " IN ORDER")
+    elif item == "".join(sorted(item, reverse=True)):
+        print(item + " REVERSE ORDER")
     else:
-        print(item, "NOT IN ORDER")
+        print(item + " NOT IN ORDER")
+      
